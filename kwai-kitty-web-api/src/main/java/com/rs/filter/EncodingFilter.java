844f480ebf552431aed2a73278f948edbbbbd315
rangyushuang <rangyushuang@kuaishou.com>
2022-07-20 02:05:45 +0800

    init project

--- kwai-kitty-web-api/src/main/java/com/rs/filter/EncodingFilter.java
+++ kwai-kitty-web-api/src/main/java/com/rs/filter/EncodingFilter.java
@@ -24,9 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 //过滤全部内容
 @WebFilter("/*")
 @Slf4j
-@Order(1)//指定过滤器的执行顺序,值越大越靠后执行
+@Order(1)  //指定过滤器的执行顺序,值越大越靠后执行,如果采用web.xml写的filter执行顺序跟书写顺序有关,而采用注解方式的是没有顺序可言的!!!!
 //@Component//无需添加此注解，在启动类添加@ServletComponentScan注解后，会自动将带有@WebFilter的注解进行注入！
-public class WebEncodingFilter implements Filter {
+public class EncodingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         log.info(filterConfig.getFilterName() + " is init !!!!");
@@ -42,16 +42,17 @@ public class WebEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filterChain)
             throws IOException, ServletException {
+        log.info("EncodingFilter before !!!!");
         HttpServletRequest request=(HttpServletRequest) req;
         HttpServletResponse response=(HttpServletResponse) resp;
         request.setCharacterEncoding("utf-8");
         response.setContentType("html/text;charset=utf-8");
-        log.info("WebEncodingFilter is working !!!!");
         filterChain.doFilter(request,response);
+        log.info("EncodingFilter after !!!!");
     }
 
     @Override
     public void destroy() {
-        log.info("WebEncodingFilter is destroyed !!!!");
+        log.info("EncodingFilter is destroyed !!!!");
     }
 }
